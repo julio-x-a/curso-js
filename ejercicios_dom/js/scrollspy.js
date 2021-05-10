7d0d57b99addb9ea7dde4a7df20a95d5842b22d2
julio <jrab997@gmail.com>
2021-05-09 22:37:17 -0500

    9/05/21

--- ejercicios_dom/js/scrollspy.js
+++ ejercicios_dom/js/scrollspy.js
@@ -1,20 +1,22 @@
 export default function scrollSpy() {
-    const $sections = document.querySelectorAll("section[data-scroll-spy]");
-    const callback = (entries) => {
-        entries.forEach(entry => {
-            if(entry.isIntersecting){
-                document.querySelector(`a[data-scroll-spy][href="#${entry.target.id}"]`).
-                classList.add('active');
-            } else {
-                document.querySelector(`a[data-scroll-spy][href="#${entry.target.id}"]`).
-                classList.remove('active');
-            }
-        })
-    }
-    const observer = new IntersectionObserver(callback, {
-        //root
-        //rootMargin: "-250px"
-        threshold: [0.5, 0.75],
+  const $sections = document.querySelectorAll('section[data-scroll-spy]');
+  const callback = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        document
+          .querySelector(`a[data-scroll-spy][href="#${entry.target.id}"]`)
+          .classList.add('active');
+      } else {
+        document
+          .querySelector(`a[data-scroll-spy][href="#${entry.target.id}"]`)
+          .classList.remove('active');
+      }
     });
-    $sections.forEach(section => observer.observe(section))
+  };
+  const observer = new IntersectionObserver(callback, {
+    //root
+    //rootMargin: "-250px"
+    threshold: [0.5, 0.75]
+  });
+  $sections.forEach((section) => observer.observe(section));
 }
--- ejercicios_dom/js/video_smart.js
+++ ejercicios_dom/js/video_smart.js
@@ -1,10 +1,23 @@
+/**
+ *Esta función encuentra todos los elementos HTML video, con 
+ el data attribute, identifica si está intersectado y se va a
+ reproducir o pausar dependiendo de esto.
+ */
+
 export default function videoSmart() {
-    const $video = document.getElementById('video-smart');
-    const callback = (entry) =>{
-      console.log(entry.isIntersecting);
-    }
-    const observer = new IntersectionObserver(callback, {
-        threshold: [0.5, 0.75]
+  const $videos = document.querySelectorAll('video[data-smartvideo]');
+  const callback = (entries) => {
+    entries.forEach((entry) => {
+      entry.isIntersecting ? entry.target.play() : entry.target.pause();
+      window.addEventListener('visibilitychange', (e) => {
+        document.visibilityState === 'visible'
+          ? entry.target.play()
+          : entry.target.pause();
+      });
     });
-    observer.observe($video);
+  };
+  const observer = new IntersectionObserver(callback, {
+    threshold: [0.5]
+  });
+  $videos.forEach((video) => observer.observe(video));
 }
