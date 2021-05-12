f7fbd50774a3bfc0e5abde5bc68826c01ca95bf2
Julio <jrab997@gmail.com>
2021-05-12 16:42:50 -0500

    narrador.js terminado

--- ejercicios_dom/js/narrador.js
+++ ejercicios_dom/js/narrador.js
@@ -1,29 +1,35 @@
 /**
- *
- * @param {String} txtArea Representa el input textarea HTML
- * @param {String} btn Representa el botón HTML
- * @param {String} select Representa el elemento Select HTML
+ * Esta función se encarga de llenar un select dinamicamente con las voces disponibles
+ * del navegador, a su vez narrara lo que se introduzca en el input de texto con la
+ * voz del select actualmente seleccionada.
+ * @param {String} inputText Representa un input de texto HTML
+ * @param {String} btn Representa un input tipo button HTML
+ * @param {String} select Representa el elemento select HTML
  */
 
-export default function narrador(txtArea, btn, select) {
-  const $txtArea = document.querySelector(txtArea);
-
+export default function narrador(inputText, btn, select) {
+  const $inputText = document.querySelector(inputText);
+  const $select = document.querySelector(select);
+  fillSelect($select);
   document.addEventListener('click', (e) => {
     if (e.target.matches(btn)) {
-      fillSelect(select);
-      let utterance = new SpeechSynthesisUtterance($txtArea.value);
-      speechSynthesis.speak(utterance);
+        let index = $select.selectedIndex;
+        let utterance = new SpeechSynthesisUtterance($inputText.value);
+        utterance.voice = speechSynthesis.getVoices()[index];
+        speechSynthesis.speak(utterance);
     }
   });
 }
 
 function fillSelect(select) {
-  const $select = document.querySelector(select);
-  const voices = speechSynthesis.getVoices();
-  voices.forEach((voice) => {
-    console.log(voice);
-    const $option = document.createElement('option');
-    $option.textContent = voice.name;
-    $select.appendChild($option);
-  });
+  let voices = [];
+  speechSynthesis.addEventListener('voiceschanged', (e) => {
+        voices = speechSynthesis.getVoices();
+        voices.forEach((voice, index) => {
+            const $option = document.createElement('option');
+            $option.textContent = voice.name;
+            $option.id = index;
+            select.append($option);
+        });
+    })
 }
